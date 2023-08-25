504a1a264c25b0f83423c9f2f76621336099032a
Doma <jang6129@naver.com>
2023-08-25 10:12:39 +0900

    refactor: Refactoring of frontend package architecture

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import logo from "../assets/images/kkini_logo.png";
+import "../styles/MainPage.css"
+function MainPage() {
+
+    return (
+        <div className="MainPage">
+            <img src={logo} className="logo" alt="kkini logo" />
+        </div>
+    )
+}
+
+export default MainPage;
