b16f71804a5363536d04c996b7ee37b0ed9d8211
이형승 <dlgudtmd1022@gamil.com>
2023-09-13 18:32:31 +0900

    feat: Introduction and Help Structure

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 import MyPage from "./pages/MyPage";
 import LoginPage from "./pages/LoginPage";
 import {UserProvider} from "./components/UserContext";
+import InformationPage from "./pages/InformationPage";
+import HelpPage from "./pages/HelpPage";
 
 function App() {
     return (
@@ -18,6 +20,8 @@ function App() {
                     <Route path="/products/:productId" element={<ProductDetailPage/>} />
                     <Route path="/user" element={<MyPage />}/>
                     <Route path="/login" element={<LoginPage />}/>
+                    <Route path="/information" element={<InformationPage />}/>
+                    <Route path="/help" element={<HelpPage />}/>
                 </Routes>
             </UserProvider>
         </BrowserRouter>
