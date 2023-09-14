3122ecd6991540318249fcdd67c4bdcdb882e44b
Doma <jang6129@naver.com>
2023-09-14 14:32:54 +0900

    Feat: Implementation of Review Create By User

--- src/components/BannerAd.js
+++ src/components/BannerAd.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/MainPage.css';
+import bannerImage from '../assets/images/banner_image.png';
 
 function BannerAd({ imageUrl, altText, linkUrl }) {
     return (
         <div className="banner-ad">
             <a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer">
-                <img src="src/assets/images/kkini_logo.png" alt="끼니 프로모션" />
+                <img src={bannerImage} alt="끼니 프로모션" />
             </a>
         </div>
     );
