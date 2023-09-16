b78630ea88a6cc51fc20ec2bdd0a32a5f7602c50
최진주 <jjipper@naver.com>
2023-09-16 23:03:20 +0100

    [메인] 반응형 퍼블리싱

--- src/components/RankingSection.js
+++ src/components/RankingSection.js
@@ -1,33 +1,52 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import icLink from "../assets/images/ic_link.png";
+import icLinkGreen from "../assets/images/ic_link_green.png";
 
 function RankingSection() {
-    return(
-        <div className="ranking-section">
-            <section className="ranking-kkini-ranking">
-                <h2>끼니 PICK</h2>
-                <p>끼니의 친구가 되어, 나를 위한 상품들을 만나보세요!</p>
-                <div className="ranking-images">
-                    {/* ... (랭킹 이미지 코드) */}
-                </div>
-            </section>
+  const navigate = useNavigate();
 
-            <section className="ranking-kkini">
-                <h2>끼니 랭킹</h2>
-                <p>현재 인기만점 제품</p>
-                <div className="ranking-images">
-                    {/* ... (랭킹 이미지 코드) */}
-                </div>
-            </section>
-
-            <section className="ranking-kkini-green">
-                <h2>끼니 그린 랭킹</h2>
-                <p>간편하고, 건강하게!</p>
-                <div className="ranking-images">
-                    {/* ... (랭킹 이미지 코드) */}
-                </div>
-            </section>
+  return (
+    <div className="ranking-section">
+      <section className="ranking-kkini-ranking">
+        <div className="tit">
+          <h2>끼니 PICK</h2>
+          {/* 로그인시 */}
+          {/* <button onClick={() => navigate("/ranking")}>
+            <img src={icLink} alt="끼니 랭킹 바로가기" />
+          </button> */}
+          {/* 비로그인시 */}
+          <button className="btn-login" onClick={() => navigate("/pick")}>
+            로그인
+          </button>
         </div>
-    );
+        <p>끼니의 친구가 되어, 나를 위한 상품들을 만나보세요!</p>
+        <div className="ranking-images">{/* ... (랭킹 이미지 코드) */}</div>
+      </section>
+      <div className="ranking-section-bg">
+        <section className="ranking-kkini">
+          <div className="tit">
+            <h2>끼니 랭킹</h2>
+            <button onClick={() => navigate("/ranking")}>
+              <img src={icLink} alt="끼니 랭킹 바로가기" />
+            </button>
+          </div>
+          <p>현재 인기만점 제품</p>
+          <div className="ranking-images">{/* ... (랭킹 이미지 코드) */}</div>
+        </section>
+      </div>
+      <section className="ranking-kkini-green">
+        <div className="tit">
+          <h2>끼니 그린 랭킹</h2>
+          <button onClick={() => navigate("/green-ranking")}>
+            <img src={icLinkGreen} alt="끼니 그린 랭킹 바로가기" />
+          </button>
+        </div>
+        <p>간편하고, 건강하게!</p>
+        <div className="ranking-images">{/* ... (랭킹 이미지 코드) */}</div>
+      </section>
+    </div>
+  );
 }
 
 export default RankingSection;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import '../styles/MainPage.css';
+import React, { useState } from "react";
+import "../styles/MainPage.css";
 import NavigationContainer from "../containers/NavigationBarContainer";
 import BannerAd from "../components/BannerAd";
 import RankingSection from "../components/RankingSection";
 import Footer from "../components/Footer";
 
-function MainPage({ searchTerm: initialSearchTerm, setSearchTerm: initialSetSearchTerm,
-                      autocompleteItems: initialAutocompleteItems,
-                      setAutocompleteItems: initialSetAutocompleteItems }) {
-    const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
-    const [autocompleteItems, setAutocompleteItems] = useState(initialAutocompleteItems);
+function MainPage({
+  searchTerm: initialSearchTerm,
+  setSearchTerm: initialSetSearchTerm,
+  autocompleteItems: initialAutocompleteItems,
+  setAutocompleteItems: initialSetAutocompleteItems,
+}) {
+  const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+  const [autocompleteItems, setAutocompleteItems] = useState(
+    initialAutocompleteItems
+  );
 
-
-    return (
-        <>
-            <NavigationContainer
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                autocompleteItems={autocompleteItems}
-                setAutocompleteItems={setAutocompleteItems}
-            />
-            <BannerAd className="banner-ad"/>
-            <RankingSection className="ranking-section"/>
-            <Footer className="footer"/>
-        </>
-    );
+  return (
+    <>
+      <NavigationContainer
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        autocompleteItems={autocompleteItems}
+        setAutocompleteItems={setAutocompleteItems}
+      />
+      <BannerAd className="banner-ad" />
+      <RankingSection className="ranking-section" />
+      <Footer className="footer" />
+    </>
+  );
 }
-
 
 export default MainPage;
