7dbe153ad16f916fe56150ca839df5e904f2abe0
최진주 <jjipper@naver.com>
2023-09-17 23:00:39 +0100

    [도움말] 반응형 퍼블리싱 완료

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,29 +1,36 @@
-import React, {useState} from "react";
-import NavigationContainer from "../containers/NavigationBarContainer"
-import Help from "../components/Help"
-import BannerAd from "../components/BannerAd"
-import Footer from "../components/Footer"
+import React, { useState } from "react";
+import "../styles/HelpPage.css";
+import NavigationContainer from "../containers/NavigationBarContainer";
+import Help from "../components/Help";
+import BannerAd from "../components/BannerAd";
+import Footer from "../components/Footer";
 
-function HelpPage ({ searchTerm: initialSearchTerm, setSearchTerm: initialSetSearchTerm,
-                              autocompleteItems: initialAutocompleteItems,
-                              setAutocompleteItems: initialSetAutocompleteItems }){
-    const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
-    const [autocompleteItems, setAutocompleteItems] = useState(initialAutocompleteItems);
+function HelpPage({
+  searchTerm: initialSearchTerm,
+  setSearchTerm: initialSetSearchTerm,
+  autocompleteItems: initialAutocompleteItems,
+  setAutocompleteItems: initialSetAutocompleteItems,
+}) {
+  const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+  const [autocompleteItems, setAutocompleteItems] = useState(
+    initialAutocompleteItems
+  );
 
-
-    return (
-        <>
-            <NavigationContainer
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                autocompleteItems={autocompleteItems}
-                setAutocompleteItems={setAutocompleteItems}
-            />
-            <BannerAd className="banner-ad"/>
-            <Help />
-            <Footer className="footer"/>
-        </>
-    );
+  return (
+    <>
+      <NavigationContainer
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        autocompleteItems={autocompleteItems}
+        setAutocompleteItems={setAutocompleteItems}
+      />
+      <div className="page-tit content-max">
+        <h1>도움말</h1>
+      </div>
+      <Help />
+      <Footer className="footer" />
+    </>
+  );
 }
 
 export default HelpPage;
