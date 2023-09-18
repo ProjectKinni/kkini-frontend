60ddf73af522b70d43a670798a1816d847a72d94
unknown <dlgudtmd1022@gmail.com>
2023-09-18 21:46:26 +0900

    feat: MyPage Like

--- src/components/LikedProductList.js
+++ src/components/LikedProductList.js
@@ -1,24 +1,3 @@
-// import LikedProductItem from "./LikedProductItem";
-//
-// function LikedProductList({ likedProducts, handleRemoveClick }) {
-//     return (
-//         <div>
-//             <h2>찜 목록</h2>
-//             {likedProducts.length === 0 ? (
-//                 <p>현재 찜한 상품이 없습니다.</p>
-//             ) : (
-//                 <ul>
-//                     {likedProducts.map((productLike) => (
-//                         <LikedProductItem key={`${productLike.product.productId}-${productLike.users.userId}`} productLike={productLike} handleRemoveClick={handleRemoveClick} />
-//                     ))}
-//                 </ul>
-//             )}
-//         </div>
-//     );
-// }
-//
-// export default LikedProductList;
-
 import React, { useState } from 'react';
 import LikedProductItem from "./LikedProductItem";
 
@@ -26,9 +5,6 @@ function LikedProductList({ likedProducts, handleRemoveClick }) {
     const [visibleItems, setVisibleItems] = useState(5);
     const [showAllItems, setShowAllItems] = useState(false);
 
-    // const handleShowMore = () => {
-    //     setVisibleItems(prevVisibleItems => prevVisibleItems + 5);
-    // };
     const handleShowMore = () => {
         setVisibleItems(prevVisibleItems => Math.min(prevVisibleItems + 5, likedProducts.length));
     };
@@ -39,69 +15,6 @@ function LikedProductList({ likedProducts, handleRemoveClick }) {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
-    // return (
-    //     <div>
-    //         <h2>찜한 상품 목록</h2>
-    //         {likedProducts.slice(0, visibleItems).map((productLike) => (
-    //             // 각각의 상품 아이템 렌더링
-    //             <LikedProductItem key={`${productLike.product.productId}-${productLike.users.userId}`} productLike={productLike} handleRemoveClick={handleRemoveClick} />
-    //         ))}
-    //         {!showAllItems && visibleItems < likedProducts.length && (
-    //             <button onClick={handleShowMore}>더 보기</button>
-    //         )}
-    //         {showAllItems && (
-    //             <button onClick={handleCollapse}>접기</button>
-    //         )}
-    //     </div>
-    // );
-    // return (
-    //     <div>
-    //         <h2>찜한 상품 목록</h2>
-    //         {likedProducts.slice(0, visibleItems).map((productLike) => (
-    //             // 각각의 상품 아이템 렌더링
-    //             <LikedProductItem key={`${productLike.product.productId}-${productLike.users.userId}`} productLike={productLike} handleRemoveClick={handleRemoveClick} />
-    //         ))}
-    //         {!showAllItems && visibleItems < likedProducts.length && (
-    //             <button onClick={handleShowMore}>더 보기</button>
-    //         )}
-    //         {/*{!showAllItems && visibleItems > 5 && (*/}
-    //         {/*    <button onClick={handleCollapse}>접기</button>*/}
-    //         {/*)}*/}
-    //         {visibleItems >= likedProducts.length && showAllItems && (
-    //             <button onClick={handleCollapse}>접기</button>
-    //         )}
-    //     </div>
-    // );
-    // return (
-    //     <div>
-    //         <h2>찜한 상품 목록</h2>
-    //         {likedProducts.slice(0, visibleItems).map((productLike) => (
-    //             // 각각의 상품 아이템 렌더링
-    //             <LikedProductItem key={`${productLike.product.productId}-${productLike.users.userId}`} productLike={productLike} handleRemoveClick={handleRemoveClick} />
-    //         ))}
-    //         {!showAllItems && visibleItems > 5 && (
-    //             <button onClick={handleCollapse}>접기</button>
-    //         )}
-    //         {!showAllItems && visibleItems < likedProducts.length && (
-    //             <button onClick={handleShowMore}>더 보기</button>
-    //         )}
-    //     </div>
-    // );
-    // return (
-    //     <div>
-    //         <h2>찜한 상품 목록</h2>
-    //         {likedProducts.slice(0, visibleItems).map((productLike) => (
-    //             // 각각의 상품 아이템 렌더링
-    //             <LikedProductItem key={`${productLike.product.productId}-${productLike.users.userId}`} productLike={productLike} handleRemoveClick={handleRemoveClick} />
-    //         ))}
-    //         {!showAllItems && visibleItems < likedProducts.length && (
-    //             <button onClick={handleShowMore}>더 보기</button>
-    //         )}
-    //         {visibleItems >= likedProducts.length && visibleItems > 5 && (
-    //             <button onClick={handleCollapse}>접기</button>
-    //         )}
-    //     </div>
-    // );
     return (
         <div>
             <h2>찜한 상품 목록</h2>
