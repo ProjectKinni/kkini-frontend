09778ad7e3bc7fcdb93df8beda564efe22356300
최진주 <jjipper@naver.com>
2023-09-19 00:35:22 +0100

    [검색결과 - 필터] accordion script 코드 추가

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from "react";
+import ArrowRight from "../assets/images/arrow_right.png";
 
 function Categories({ onCategoryChange, searchTerm }) {
   const [selectedCategories, setSelectedCategories] = useState([]);
+
+  // 0919 최진주 작성 Accordion 추가
+  const [accordionOpen, setAccordionOpen] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 768) {
+        setAccordionOpen(false);
+      } else {
+        setAccordionOpen(true);
+      }
+    }
+
+    // 컴포넌트가 마운트될 때 한 번 실행하고, 창 크기가 변경될 때마다 실행합니다.
+    handleResize(); // 초기 설정
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleAccordion = () => {
+    setAccordionOpen(!accordionOpen);
+  };
+  // 0919 최진주 작성 Accordion 여기까지
 
   useEffect(() => {
     onCategoryChange(selectedCategories);
@@ -29,10 +57,15 @@ function Categories({ onCategoryChange, searchTerm }) {
   };
 
   return (
-    <div className="category-wrap">
-      <h3>카테고리</h3>
+    <div className={`category-wrap accordion ${accordionOpen ? "on" : "off"}`}>
+      <div className="accordion-tit">
+        <h3>카테고리</h3>
+        <button onClick={toggleAccordion}>
+          <img src={ArrowRight} alt={accordionOpen ? "닫기" : "열기"} />
+        </button>
+      </div>
       {["간식", "육가공", "음료", "즉석섭취식품"].map((category) => (
-        <div className="categories" key={category}>
+        <div className="categories icon" key={category}>
           <input
             id={category}
             type="checkbox"
--- src/components/KkiniChecked.js
+++ src/components/KkiniChecked.js
@@ -1,7 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ArrowRight from "../assets/images/arrow_right.png";
 
 function KkiniChecked({ onKkiniChecked }) {
   const [isChecked, setIsChecked] = useState(false);
+
+  // 0919 최진주 작성 Accordion 추가
+  const [accordionOpen, setAccordionOpen] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 768) {
+        setAccordionOpen(false);
+      } else {
+        setAccordionOpen(true);
+      }
+    }
+
+    // 컴포넌트가 마운트될 때 한 번 실행하고, 창 크기가 변경될 때마다 실행합니다.
+    handleResize(); // 초기 설정
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleAccordion = () => {
+    setAccordionOpen(!accordionOpen);
+  };
+  // 0919 최진주 작성 Accordion 여기까지
 
   const handleKkiniChecked = (checkedValue) => {
     setIsChecked(checkedValue);
@@ -9,15 +37,22 @@ function KkiniChecked({ onKkiniChecked }) {
   };
 
   return (
-    <div className="category-wrap">
-      <h3>끼니 그린 체크</h3>
-      <input
-        id="green"
-        type="checkbox"
-        checked={isChecked}
-        onChange={(e) => handleKkiniChecked(e.target.checked)}
-      />
-      <label for="green">Kkini Green Checked</label>
+    <div className={`category-wrap accordion ${accordionOpen ? "on" : "off"}`}>
+      <div className="accordion-tit">
+        <h3>끼니 그린 체크</h3>
+        <button onClick={toggleAccordion}>
+          <img src={ArrowRight} alt={accordionOpen ? "닫기" : "열기"} />
+        </button>
+      </div>
+      <div className="categories icon green">
+        <input
+          id="green"
+          type="checkbox"
+          checked={isChecked}
+          onChange={(e) => handleKkiniChecked(e.target.checked)}
+        />
+        <label for="green">끼니 그린</label>
+      </div>
     </div>
   );
 }
