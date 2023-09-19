7037e308b331d2ecf626044e28326633468f3729
최진주 <jjipper@naver.com>
2023-09-19 22:08:18 +0100

    [메인] 반응형 퍼블리싱 완료

--- src/components/RankingSection.js
+++ src/components/RankingSection.js
@@ -1,25 +1,35 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import MainSliderSection from "../components/mainPage/MainSliderSection"
+import MainSliderSection from "../components/mainPage/MainSliderSection";
 
 function RankingSection() {
   const navigate = useNavigate();
 
   //로그인 여부를 확인하는 로직 추가를 여기에 해야 하나요. 어떻게 하는지는 나도 몰라요.
-    //MainSliderSection에 isLoggedIn props 있어요.
-    // 일단 false로 지정해놓을게요.
+  //MainSliderSection에 isLoggedIn props 있어요.
+  // 일단 false로 지정해놓을게요.
   const isLoggedIn = false;
 
   return (
-
-      <div className="ranking-section">
-
-          <MainSliderSection type="pick" className="ranking-kkini-ranking" isLoggedIn={isLoggedIn}/>
-          <MainSliderSection type="ranking" className="ranking-kkini" isLoggedIn={isLoggedIn}/>
-          <MainSliderSection type="green" className="ranking-kkini-green" isLoggedIn={isLoggedIn}/>
-
+    <div className="ranking-section">
+      <MainSliderSection
+        type="pick"
+        className="ranking-kkini-ranking content-max"
+        isLoggedIn={isLoggedIn}
+      />
+      <div className="bg-grey">
+        <MainSliderSection
+          type="ranking"
+          className="ranking-kkini content-max"
+          isLoggedIn={isLoggedIn}
+        />
       </div>
-
+      <MainSliderSection
+        type="green"
+        className="ranking-kkini-green content-max"
+        isLoggedIn={isLoggedIn}
+      />
+    </div>
   );
 }
 
