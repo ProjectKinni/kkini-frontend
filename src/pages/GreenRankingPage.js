c708efc7792a13ccef26466ce9030d8c76d3f3b0
최진주 <jjipper@naver.com>
2023-09-19 23:38:38 +0100

    [랭킹 리스트] max width 및 반응형 적용되도록 html 구조 변

--- src/pages/GreenRankingPage.js
+++ src/pages/GreenRankingPage.js
@@ -5,70 +5,64 @@ import Help from "../components/Help";
 import BannerAd from "../components/BannerAd";
 import Footer from "../components/Footer";
 
-
 function GreenRankingPage({
   searchTerm: initialSearchTerm,
   setSearchTerm: initialSetSearchTerm,
   autocompleteItems: initialAutocompleteItems,
   setAutocompleteItems: initialSetAutocompleteItems,
-}){
-    const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
-    const [autocompleteItems, setAutocompleteItems] = useState(
-        initialAutocompleteItems
-    );
+}) {
+  const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+  const [autocompleteItems, setAutocompleteItems] = useState(
+    initialAutocompleteItems
+  );
 
-    const [kkiniGreenCheck, setKkiniGreenCheck] = useState(false);
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [filters, setFilters] = useState({
-        isLowCalorie: false,
-        isSugarFree: false,
-        isLowSugar: false,
-        isLowCarb: false,
-        isKeto: false,
-        isTransFat: false,
-        isHighProtein: false,
-        isLowSodium: false,
-        isCholesterol: false,
-        isSaturatedFat: false,
-        isLowFat: false,
-    });
-    const handleFilterChange = (updatedFilters) => {
-        setFilters(updatedFilters);
-    };
-    const handleKkiniGreenCheckChange = (value) => {
-        setKkiniGreenCheck(value);
-    };
-    
+  const [kkiniGreenCheck, setKkiniGreenCheck] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [filters, setFilters] = useState({
+    isLowCalorie: false,
+    isSugarFree: false,
+    isLowSugar: false,
+    isLowCarb: false,
+    isKeto: false,
+    isTransFat: false,
+    isHighProtein: false,
+    isLowSodium: false,
+    isCholesterol: false,
+    isSaturatedFat: false,
+    isLowFat: false,
+  });
+  const handleFilterChange = (updatedFilters) => {
+    setFilters(updatedFilters);
+  };
+  const handleKkiniGreenCheckChange = (value) => {
+    setKkiniGreenCheck(value);
+  };
 
-    return(
-        <>
-            {/*기본속성*/}
-            <NavigationContainer
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                autocompleteItems={autocompleteItems}
-                setAutocompleteItems={setAutocompleteItems}
-            />
-            
-            <div className="page-tit content-max">
-                <h1>끼니 그린 랭킹</h1>
-                <p>
-                    간편하게, 건강하게!
-                </p>
-            </div>
-
-            <CategoryBarContainer
-                onKkiniChecked={handleKkiniGreenCheckChange}
-                onCategoryChange={setSelectedCategories}
-                onFilterChange={handleFilterChange}
-                
-                filters={filters}
-                kkiniGreenCheck={kkiniGreenCheck}
-            />
-
-
-            <Footer className="footer" />
-        </>
-    );
+  return (
+    <>
+      {/*기본속성*/}
+      <NavigationContainer
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        autocompleteItems={autocompleteItems}
+        setAutocompleteItems={setAutocompleteItems}
+      />
+      <div className="page-tit content-max green">
+        <h1>끼니 그린 랭킹</h1>
+        <p>간편하게, 건강하게!</p>
+      </div>
+      <div className="product-wrap content-max">
+        <CategoryBarContainer
+          onKkiniChecked={handleKkiniGreenCheckChange}
+          onCategoryChange={setSelectedCategories}
+          onFilterChange={handleFilterChange}
+          filters={filters}
+          kkiniGreenCheck={kkiniGreenCheck}
+        />
+        <div className="product-list-wrapper">{/* <ProductList /> */}</div>
+      </div>
+      <Footer className="footer" />
+    </>
+  );
 }
 export default GreenRankingPage;
