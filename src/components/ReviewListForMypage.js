897a1861bbe9aaf881c397556b7aaa2b57ed4d64
이형승 <dlgudtmd1022@gamil.com>
2023-09-20 19:49:31 +0900

    feat: MyPage ReviewList

--- src/components/ReviewListForMypage.js
+++ src/components/ReviewListForMypage.js
@@ -1,62 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useUser } from './UserContext';
-import axios from 'axios';
-
-const SERVER_URL = "http://localhost:8080";
+import {useUserReviews} from "./UseUserReviews";
 
 function UserReviews() {
     const { user } = useUser();
-    const [reviews, setReviews] = useState([]);
-    const [page, setPage] = useState(0);
+    const PAGE_GROUP_SIZE = 10;
+    const { reviewsData, page, setPage , deleteAndFetchAgain} = useUserReviews(user.userId);
 
-    useEffect(() => {
-
-        // 유저 ID와 페이지 번호를 사용하여 리뷰 목록을 가져옵니다.
-        axios.get(`${SERVER_URL}/reviews/users/${user.userId}?page=${page}`)
-            .then(response => {
-                setReviews(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching user reviews:', error);
-
-            });
-    }, [user, page]);
-
-    const handleDeleteReview = (reviewId) => {
-        // 리뷰 삭제 요청을 서버에 보냅니다.
-        axios.delete(`${SERVER_URL}/reviews/${reviewId}`)
-            .then(response => {
-                // 삭제 성공한 경우, 삭제한 리뷰를 화면에서도 제거합니다.
-                setReviews(prevReviews => prevReviews.filter(review => review.reviewId !== reviewId));
-            })
-            .catch(error => {
-                console.error('Error deleting review:', error);
-            });
-    };
 
     return (
         <div>
-            {reviews.length === 0 ? (
+            {reviewsData.content.length === 0 ? (
                 <p>현재 작성된 리뷰가 없습니다.</p>
             ) : (
-                <ul>
-                    {reviews.map(review => (
-                        <li key={review.reviewId}>
-                            <br/>
-                            <p>작성자: {review.user.nickname}</p>
-                            <p>리뷰 내용: {review.content}</p>
-                            <p>상품 이미지: <img src={review.product.image} alt={review.product.productName} /></p>
-                            <p>상품 이름: {review.product.productName}</p>
-                            <button onClick={() => handleDeleteReview(review.reviewId)}>리뷰 삭제 </button>
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <ul>
+                        {reviewsData.content.map(review => (
+                            <li key={review.reviewId}>
+                                <br/>
+                                <p>작성자: {review.user.nickname}</p>
+                                <p>리뷰 내용: {review.content}</p>
+                                {/*<img src={review.product.image} alt={review.product.productName} />*/}
+                                <p>상품 이름: {review.product.productName}</p>
+                                <button onClick={() => deleteAndFetchAgain(review.reviewId)}>리뷰 삭제</button>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {page > 0 && <button onClick={() => setPage(page - 1)}>이전</button>}
+
+                        {Array.from({ length: PAGE_GROUP_SIZE }, (_, i) => {
+                            const pageNumber = Math.floor(page / PAGE_GROUP_SIZE) * PAGE_GROUP_SIZE + i;
+                            if (pageNumber >= reviewsData.totalPages) return null;
+                            return (
+                                <button key={i} onClick={() => setPage(pageNumber)} disabled={page === pageNumber}>
+                                    {pageNumber + 1}
+                                </button>
+                            );
+                        })}
+
+                        {(page + 1) !== reviewsData.totalPages &&
+                            (<button onClick={() => setPage(page + 1)}>다음</button>)}
+                    </ul>
+                </>
             )}
-            {/* 페이지 이동 버튼 등을 추가하여 페이지를 변경할 수 있도록 함 */}
-            <button onClick={() => setPage(page - 1)}>이전</button>
-            <button onClick={() => setPage(page + 1)}>다음</button>
         </div>
     );
 }
 
 export default UserReviews;
+
