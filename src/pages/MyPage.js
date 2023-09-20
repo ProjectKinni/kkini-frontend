ee5424b5a977949f41ce6c3957f65ba1c4809270
이형승 <dlgudtmd1022@gamil.com>
2023-09-20 19:53:30 +0900

    feat: MyPage ReviewList

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,37 +1,3 @@
-// import React, {useEffect, useState} from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import ShowUserInfo from "../components/ShowUserInfo";
-// import handleDeleteClick from "../components/HandleDeleteClick";
-// import {fetchLikedProducts} from "../components/UseFetchLikedProducts";
-// import LikedProductList from "../components/LikedProductList";
-// import {useUser} from "../components/UserContext";
-// import useRemoveLike from "../components/UseRemoveLike";
-//
-// function MyPage() {
-//     const [isEditingNickname, setIsEditingNickname] = useState(false);
-//     const navigate = useNavigate();
-//     const { user } = useUser();
-//     const [likedProducts, setLikedProducts] = useState([]);
-//     const removeLike = useRemoveLike(user, setLikedProducts); // 가져온 함수 사용
-//
-//     useEffect(() => {
-//         fetchLikedProducts(user, setLikedProducts);
-//     }, [user]);
-//
-//     return (
-//         <>
-//             <ShowUserInfo
-//                 isEditingNickname={isEditingNickname}
-//                 setIsEditingNickname={setIsEditingNickname}
-//                 handleDeleteClick={() => handleDeleteClick(() => navigate('/'))}
-//             />
-//             <LikedProductList likedProducts={likedProducts} handleRemoveClick={removeLike} />
-//         </>
-//     );
-// }
-//
-// export default MyPage;
-
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ShowUserInfo from "../components/ShowUserInfo";
