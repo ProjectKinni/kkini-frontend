7b78c33f06542b383b1ecd22d29ac58e5d84d554
Jayyyyp <pjae9726@gmail.com>
2023-09-20 12:46:55 +0900

    KN-198

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -40,10 +40,8 @@ const ProductDetail = () => {
                     )}
                 </div>
                 <h2>평점: ⭐{product.averageRating}</h2>
-                <h2>상품번호: {product.productId}</h2>
                 <h3>조회수: {viewCount || '0'}</h3>
-                <h3>랭킹: </h3>
-                <input type="submit" value="리뷰적기" />
+                <h3>랭킹: {product.categoryName} 중, ??위</h3>
                 <h3>중량: {product.servingSize}g</h3>
                 <h2>열량: {product.kcal}kcal</h2>
 
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -28,11 +28,13 @@ const ReviewForm = ({ userId, productId, onSubmit }) => {
             <h2>리뷰 작성하기</h2>
             <div className="form-group">
                 <label>평점</label>
-                <input type="number" value={rating} onChange={(e) => setRating(e.target.value)} />
+                <input type="number" value={rating}
+                       onChange={(e) => setRating(e.target.value)} />
             </div>
             <div className="form-group">
                 <label>내용</label>
-                <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+                <textarea value={content}
+                          onChange={(e) => setContent(e.target.value)} />
             </div>
             <button onClick={handleSubmit}>리뷰 작성</button>
         </div>
