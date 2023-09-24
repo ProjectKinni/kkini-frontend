c5604d1f87ece22b34b3f45f046c73b6c539594d
최진주 <jjipper@naver.com>
2023-09-24 18:18:10 +0100

    [마이페이지] 리뷰 목록 css 스타일 적용, 페이지네이션 스타일 적용

--- src/components/ReviewListForMypage.js
+++ src/components/ReviewListForMypage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useUser } from './UserContext';
+import IcStar from "../assets/images/star_on.png";
+import IcStarOff from "../assets/images/star_off.png";
 import {useUserReviews} from "./UseUserReviews";
 import {Link} from "react-router-dom";
 
@@ -13,41 +15,76 @@ function UserReviews() {
     };
 
     return (
-        <div>
+        <div className="my-page-con content-max">
             {reviewsData.content.length === 0 ? (
-                <p>현재 작성된 리뷰가 없습니다.</p>
+                <p className='no-data'>현재 작성된 리뷰가 없습니다.</p>
             ) : (
                 <>
-                    <ul>
+                    <ul className="review-list">
                         {reviewsData.content.map(review => (
-                            <li key={review.reviewId}>
-                                <br/>
-                                <img src={review.product.image} alt={review.product.productName} />
-                                <Link to={`/products/${review.product.productId}`}>
-                                   <p>상품 이름: {review.product.productName}</p>
-                                </Link>
-                                <p>평점 : {review.product.averageRating}</p>
-                                <p>리뷰 내용: {review.content}</p>
-                                <button onClick={() => deleteAndFetchAgain(review.reviewId)}>리뷰 삭제</button>
+                            <li className="review-card" key={review.reviewId}>
+                                <div className="img-wrapper">
+                                    <img src={review.product.image} alt={review.product.productName} />
+                                </div>
+                                <div className='con-wrapper'>
+                                    <Link to={`/products/${review.product.productId}`} className="product-name">
+                                    {review.product.productName}
+                                    </Link>
+                                    <div className='my-review'>
+                                        <div className="rating-display">
+                                            {Array.from({ length: 5 }, (_, i) => (
+                                                <img
+                                                    src={i < review.product.averageRating ? IcStar : IcStarOff}
+                                                    key={i}
+                                                />
+                                            ))}
+                                        </div>
+                                        <button className='ic-like ic-delete-like' onClick={() => deleteAndFetchAgain(review.reviewId)}>리뷰 삭제</button>
+                                    </div>
+                                    <p>{review.content}</p>
+                                    <div className="review-image-wrap">
+                                        {/* 이미지가 있다면 띄워주기 */}
+                                        {review.image1 && (
+                                            <div className="img-wrapper review-image">
+                                                <img src={review.image1}/>
+                                            </div>
+                                        )}
+                                        {review.image2 && (
+                                            <div className="img-wrapper review-image">
+                                                <img src={review.image2}/>
+                                            </div>
+                                        )}
+                                        {review.image3 && (
+                                            <div className="img-wrapper review-image">
+                                                <img src={review.image3}/>
+                                            </div>
+                                        )}
+                                        {review.image4 && (
+                                            <div className="img-wrapper review-image">
+                                                <img src={review.image4}/>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
-                    <ul>
+                    <div className='pagination'>
                         {page > 0 && <button onClick={() => handlePageChange(page - 1)}>이전</button>}
-
-                        {Array.from({ length: PAGE_GROUP_SIZE }, (_, i) => {
-                            const pageNumber = Math.floor(page / PAGE_GROUP_SIZE) * PAGE_GROUP_SIZE + i;
-                            if (pageNumber >= reviewsData.totalPages) return null;
-                            return (
-                                <button key={i} onClick={() => handlePageChange(pageNumber)} disabled={page === pageNumber}>
-                                    {pageNumber + 1}
-                                </button>
-                            );
-                        })}
-
+                        <div className='pages'>
+                            {Array.from({ length: PAGE_GROUP_SIZE }, (_, i) => {
+                                const pageNumber = Math.floor(page / PAGE_GROUP_SIZE) * PAGE_GROUP_SIZE + i;
+                                if (pageNumber >= reviewsData.totalPages) return null;
+                                return (
+                                    <button key={i} onClick={() => handlePageChange(pageNumber)} disabled={page === pageNumber}>
+                                        {pageNumber + 1}
+                                    </button>
+                                );
+                            })}
+                        </div>
                         {(page + 1) !== reviewsData.totalPages &&
                             (<button onClick={() => handlePageChange(page + 1)}>다음</button>)}
-                    </ul>
+                    </div>
                 </>
             )}
         </div>
